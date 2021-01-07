e5d4780f2fe50531a3e00a1fd6820ec713bc5911
赵佳成 <admin@g497813927.com>
2021-01-08 03:22:57 +0800

    Feat: error process for macOS dynamics get
    Output error to the console for macOS

--- dynamic_topic_watch_macOS.py
+++ dynamic_topic_watch_macOS.py
@@ -46,49 +46,52 @@ print('开始监视话题<{}>的更新...'.format(topic_name))
 print()
 
 while True:
-    new_dynamics = topicwh.get_update()
-    if len(new_dynamics) > 0:
-        for dynamic in new_dynamics:
-            dynamic_id = dynamic['desc']['dynamic_id']
-            dynamic_type = str(dynamic['desc']['type'])
-            dynamic_uploader_uid = str(dynamic['desc']['user_profile']['info']['uid'])
-            dynamic_uploader_name = str(dynamic['desc']['user_profile']['info']['uname'])
-            content = "未解析"
-            type_contains_title = False
-            title = ""
-            if dynamic_type in dynamic_types['types']:
-                type_data = dynamic_types['types'][dynamic_type]
-                type_name = type_data['name']
-                type_contains_title = type_data['contains_title']
-                type_content_path = type_data['path']
-                content = dynamic.copy()
-                for k in type_content_path:
-                    content = content[k]
+    try:
+        new_dynamics = topicwh.get_update()
+        if len(new_dynamics) > 0:
+            for dynamic in new_dynamics:
+                dynamic_id = dynamic['desc']['dynamic_id']
+                dynamic_type = str(dynamic['desc']['type'])
+                dynamic_uploader_uid = str(dynamic['desc']['user_profile']['info']['uid'])
+                dynamic_uploader_name = str(dynamic['desc']['user_profile']['info']['uname'])
+                content = "未解析"
+                type_contains_title = False
+                title = ""
+                if dynamic_type in dynamic_types['types']:
+                    type_data = dynamic_types['types'][dynamic_type]
+                    type_name = type_data['name']
+                    type_contains_title = type_data['contains_title']
+                    type_content_path = type_data['path']
+                    content = dynamic.copy()
+                    for k in type_content_path:
+                        content = content[k]
+                    if type_contains_title:
+                        type_title_path = type_data['title_path']
+                        title = dynamic.copy()
+                        for k in type_title_path:
+                            title = title[k]
+
+                else:
+                    type_name = '[未知类型动态]'
+                    diagnosis['diagnosis'].append(dynamic)
+                    with open('diagnosis.json', 'w') as dump_file:
+                        json.dump(diagnosis, dump_file)
+                        print('发现不能被识别的动态类型，请将"diagnosis.json"提交给开发者')
+                title_announce = '您关注的话题<{}>有新的{}'.format(topic_name, type_name)
+                print(title_announce)
+                print('动态链接: \n\thttps://t.bilibili.com/{}'.format(dynamic_id))
+                print('发布人: {}\tUID:{}'.format(dynamic_uploader_name, dynamic_uploader_uid))
+                print('动态内容: \n\t{}'.format(content))
                 if type_contains_title:
-                    type_title_path = type_data['title_path']
-                    title = dynamic.copy()
-                    for k in type_title_path:
-                        title = title[k]
-
-            else:
-                type_name = '[未知类型动态]'
-                diagnosis['diagnosis'].append(dynamic)
-                with open('diagnosis.json', 'w') as dump_file:
-                    json.dump(diagnosis, dump_file)
-                    print('发现不能被识别的动态类型，请将"diagnosis.json"提交给开发者')
-            title_announce = '您关注的话题<{}>有新的{}'.format(topic_name, type_name)
-            print(title_announce)
-            print('动态链接: \n\thttps://t.bilibili.com/{}'.format(dynamic_id))
-            print('发布人: {}\tUID:{}'.format(dynamic_uploader_name, dynamic_uploader_uid))
-            print('动态内容: \n\t{}'.format(content))
-            if type_contains_title:
-                print('稿件标题：\n\t{}'.format(title))
-            print()
-            if use_tts:
-                os.system('osascript -e \'display notification "{}" with title "{}"\''.format(content, title_announce))
-                os.system('osascript -e \'say "您关注的话题有新的{}"\''.format(type_name))
-                os.system('osascript -e \'say "动态内容：{}"\''.format(content))
-            else:
-                os.system('osascript -e \'display notification "{}" with title "{}"\' sound name \"Purr\"'.format(content, title_announce))
-
-    time.sleep(30)
+                    print('稿件标题：\n\t{}'.format(title))
+                print()
+                if use_tts:
+                    os.system('osascript -e \'display notification "{}" with title "{}"\''.format(content, title_announce))
+                    os.system('osascript -e \'say "您关注的话题有新的{}"\''.format(type_name))
+                    os.system('osascript -e \'say "动态内容：{}"\''.format(content))
+                else:
+                    os.system('osascript -e \'display notification "{}" with title "{}"\' sound name \"Purr\"'.format(content, title_announce))
+    except Exception as e:
+        print(e)
+    finally:
+        time.sleep(30)
