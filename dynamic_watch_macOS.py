3017096b5472075e14d75148fcb99ec3f8489f29
赵佳成 <admin@g497813927.com>
2021-01-11 18:08:12 +0800

    Fix: refresh module for topic_dynamic & Feat: error process for macOS dynamics get  (#3)
    * Fix: refresh module for topic_dynamic
    
    * Feat: error process for macOS dynamics get
    
    Output error to the console for macOS
    
    * Use ID for database query
    
    refresh_dynamic_status
    
    Co-authored-by: 王鼎文 <wangdingwen32@gmail.com>

--- dynamic_watch_macOS.py
+++ dynamic_watch_macOS.py
@@ -46,45 +46,48 @@ print('开始监视UP主<{}>的更新...'.format(uploader_name))
 print()
 
 while True:
-    new_dynamics = upwh.get_update()
-    if len(new_dynamics) > 0:
-        for dynamic in new_dynamics:
-            dynamic_id = dynamic['desc']['dynamic_id']
-            dynamic_type = str(dynamic['desc']['type'])
-            content = "未解析"
-            type_contains_title = False
-            title = ""
-            if dynamic_type in dynamic_types['types']:
-                type_data = dynamic_types['types'][dynamic_type]
-                type_name = type_data['name']
-                type_contains_title = type_data['contains_title']
-                type_content_path = type_data['path']
-                content = dynamic.copy()
-                for k in type_content_path:
-                    content = content[k]
+    try:
+        new_dynamics = upwh.get_update()
+        if len(new_dynamics) > 0:
+            for dynamic in new_dynamics:
+                dynamic_id = dynamic['desc']['dynamic_id']
+                dynamic_type = str(dynamic['desc']['type'])
+                content = "未解析"
+                type_contains_title = False
+                title = ""
+                if dynamic_type in dynamic_types['types']:
+                    type_data = dynamic_types['types'][dynamic_type]
+                    type_name = type_data['name']
+                    type_contains_title = type_data['contains_title']
+                    type_content_path = type_data['path']
+                    content = dynamic.copy()
+                    for k in type_content_path:
+                        content = content[k]
+                    if type_contains_title:
+                        type_title_path = type_data['title_path']
+                        title = dynamic.copy()
+                        for k in type_title_path:
+                            title = title[k]
+
+                else:
+                    type_name = '[未知类型动态]'
+                    diagnosis['diagnosis'].append(dynamic)
+                    with open('diagnosis.json', 'w') as dump_file:
+                        json.dump(diagnosis, dump_file)
+                        print('发现不能被识别的动态类型，请将"diagnosis.json"提交给开发者')
+                title_announce = '您关注的UP主<{}>发布了新的{}'.format(uploader_name, type_name)
+                print(title_announce)
+                print('动态ID: {}'.format(dynamic_id))
+                print('动态内容: {}'.format(content))
                 if type_contains_title:
-                    type_title_path = type_data['title_path']
-                    title = dynamic.copy()
-                    for k in type_title_path:
-                        title = title[k]
-
-            else:
-                type_name = '[未知类型动态]'
-                diagnosis['diagnosis'].append(dynamic)
-                with open('diagnosis.json', 'w') as dump_file:
-                    json.dump(diagnosis, dump_file)
-                    print('发现不能被识别的动态类型，请将"diagnosis.json"提交给开发者')
-            title_announce = '您关注的UP主<{}>发布了新的{}'.format(uploader_name, type_name)
-            print(title_announce)
-            print('动态ID: {}'.format(dynamic_id))
-            print('动态内容: {}'.format(content))
-            if type_contains_title:
-                print('稿件标题：{}'.format(title))
-            if use_tts:
-                os.system('osascript -e \'display notification "{}" with title "{}"\''.format(content, title_announce))
-                os.system('osascript -e \'say "您关注的UP主发布了新的{}"\''.format(type_name))
-                os.system('osascript -e \'say "动态内容：{}"\''.format(content))
-            else:
-                os.system('osascript -e \'display notification "{}" with title "{}"\' sound name \"Purr\"'.format(content, title_announce))
-
-    time.sleep(60)
+                    print('稿件标题：{}'.format(title))
+                if use_tts:
+                    os.system('osascript -e \'display notification "{}" with title "{}"\''.format(content, title_announce))
+                    os.system('osascript -e \'say "您关注的UP主发布了新的{}"\''.format(type_name))
+                    os.system('osascript -e \'say "动态内容：{}"\''.format(content))
+                else:
+                    os.system('osascript -e \'display notification "{}" with title "{}"\' sound name \"Purr\"'.format(content, title_announce))
+    except Exception as e:
+        print(e)
+    finally:
+        time.sleep(30)
