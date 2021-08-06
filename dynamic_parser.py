f18affa4dc086dd0342b88f12434fee29798c62b
Dingwen Wang <wangdingwen07@gmail.com>
2021-08-06 22:36:44 +0800

    Update dynamic_parser.py

--- dynamic_parser.py
+++ dynamic_parser.py
@@ -1,9 +1,13 @@
 
 class DynamicParser:
     @staticmethod
+    def plaintext_parser(dynamic) -> str:
+        return str(dynamic)
+
+    @staticmethod
     def markdown_parser(dynamic) -> str:
-        pass
+        return str(dynamic)
 
     @staticmethod
     def html_parser(dynamic) -> str:
-        pass
+        return str(dynamic)
